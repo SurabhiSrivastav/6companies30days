ba14111f5175c9fa91fc28a0c187a54929cc13c0
Surabhi Srivastava <72458190+SurabhiSrivastav@users.noreply.github.com>
2023-01-29 00:57:58 +0530

    Create strictly_palindromic_number.cpp

---
+++ Google/strictly_palindromic_number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool  palindrome(int n)
+    {
+        int ans=0;
+         while(n>0)
+         {
+             int d=n%10;
+             ans=(d*10)+ans;
+             n=n/10;
+         }
+        if(ans==n)
+        {
+            return true;
+        }
+        return false;
+    }
+    bool isStrictlyPalindromic(int n) {
+        return palindrome(n);
+    }
+};
