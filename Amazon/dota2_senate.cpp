53473339742c12a2b8342605ad8dbfb1ad73a54d
Surabhi Srivastava <72458190+SurabhiSrivastav@users.noreply.github.com>
2023-01-29 21:13:58 +0530

    Create dota2_senate.cpp

---
+++ Amazon/dota2_senate.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n=senate.length();
+        queue<int>r,d;
+        for(int i=0;i<n;i++){
+            senate[i]=='R'? r.push(i):d.push(i);
+        }
+       int ra,da;
+       while(!r.empty() && !d.empty()){
+           ra=r.front();
+           da=d.front();
+           r.pop();
+           d.pop();
+           if(ra<da) r.push(ra+n);
+           else d.push(da+n);
+       }
+       if(r.size()>d.size()) return "Radiant";
+       else return "Dire";
+    }
+};
